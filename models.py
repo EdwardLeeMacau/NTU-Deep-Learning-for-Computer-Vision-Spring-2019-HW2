dba09aa6616cb51da1b25f5e6bc54056a8f4790a
Edward Lee <david1998zzz@gmail.com>
2019-04-15 00:07:38 +0800

    Update YOLOv1 model class in models.py

--- models.py
+++ models.py
@@ -22,8 +22,13 @@ class VGG(nn.Module):
         self.image_size = image_size
 
         self.yolo = nn.Sequential(
-            nn.Linear(25088, 4096), 
-            nn.Linear(4096, 1274)
+            nn.Linear(25088, 4096),
+            nn.LeakyReLU(negative_slope=0.02),
+            nn.Dropout(0.5),
+
+            nn.Linear(4096, 1274),
+            nn.ReLU(),
+            nn.Dropout(0.5),
         )
         self._initialize_weights()
 
